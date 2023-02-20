33bd082002a1f6a28206c26f8e0ea8f1b812e33c
OHSEAN <110371295+MatheGoD@users.noreply.github.com>
2023-02-20 12:30:36 +0900

    Time: 70 ms (59.28%), Space: 43.2 MB (51.72%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,13 @@
+function numberOfSteps(num: number): number {
+  let count = 0
+  while(num !== 0){
+      if(num % 2 === 0){
+          num = num/2;
+          count++;
+      } else {
+          num--;
+          count++;
+      }
+  }
+  return count  
+};
