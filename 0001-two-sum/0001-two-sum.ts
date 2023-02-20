e4c37730223409090a7217c37ce25f2761d088f8
OHSEAN <110371295+MatheGoD@users.noreply.github.com>
2023-02-20 12:12:34 +0900

    Time: 78 ms (75.18%), Space: 45.1 MB (47.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,8 +1,13 @@
-function twoSum(nums: number[], target: number): number[] {
-    for(let i = 0 ; i < nums.length ; i++){
-        let newTarget = target - nums[i];
-        for(let j = i; j < nums.length ; j++){
-            if(nums[j+1] === newTarget) return [i,j+1]
-        }
+ function twoSum(nums: number[], target: number): number[] {
+    const map = {}
+    for (let index = 0; index < nums.length; index++) {
+      const element = nums[index]
+      const complement = target - element
+      if (map[complement] !== undefined) {
+        return [map[complement], index]
+      } else {
+        map[element] = index
+      }
     }
-};
+    return []
+ }
