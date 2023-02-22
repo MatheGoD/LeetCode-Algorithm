90caac1139d5ff36c3c008ba8db4f23fea5595b8
OHSEAN <110371295+MatheGoD@users.noreply.github.com>
2023-02-22 10:17:59 +0900

    Time: 82 ms (18.18%), Space: 44.6 MB (9.09%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.ts
+++ 1037-valid-boomerang/1037-valid-boomerang.ts
@@ -6,8 +6,5 @@ function isBoomerang(points: number[][]): boolean {
     const y2 = points[1][1]
     const y3 = points[2][1]
     
-    const firstSlope = (y2-y1)/(x2-x1)
-    const secondSlope = (y3-y2)/(x3-x2)
-    
     return (x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)!==0) 
 };
