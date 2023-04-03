85d12e45069e36090a526223c7191504b68e9bca
OHSEAN <110371295+MatheGoD@users.noreply.github.com>
2023-04-03 15:20:38 +0900

    Time: 325 ms (57.14%), Space: 75.8 MB (57.14%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.ts
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.ts
@@ -2,14 +2,27 @@ function successfulPairs(spells: number[], potions: number[], success: number): 
   const answer = [];
   potions.sort((a,b) => a-b)
   for (let i = 0 ; i < spells.length ; i++){
-    let successful = 0;
-    for (let j = 0 ; j < potions.length ; j++){
-      if(spells[i]*potions[j] >= success) {
-        successful = potions.length - j
-        break;
-      }
-    }
+    const leastPotionStrength = Math.ceil(success/spells[i]);
+    const successful = potions.length - findIndex(potions,leastPotionStrength)
     answer.push(successful)
   }
   return answer
 };
+
+function findIndex(potions : number[], target: number) :number {
+  let left : number = 0;
+  let right : number = potions.length - 1;
+  let mid : number;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (potions[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
