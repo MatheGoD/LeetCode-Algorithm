20f9b27e9d16cdcaab3114e5fc8a142335e4a750
OHSEAN <110371295+MatheGoD@users.noreply.github.com>
2024-04-04 09:31:19 +0900

    Time: 57 ms (71.95%), Space: 52 MB (15.85%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.ts
@@ -0,0 +1,17 @@
+function maxDepth(s: string): number {
+    let count = 0;
+    let maxNum = 0;
+    
+    for (let c of s) {
+        if (c === '(') {
+            count++;
+            if (maxNum < count) {
+                maxNum = count;
+            }
+        } else if (c === ')') {
+            count--;
+        }
+    }
+    
+    return maxNum;
+}
